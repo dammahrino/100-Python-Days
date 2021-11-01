673fc130a499e42885ad19f5d0d321079007fe7e
Daniel Cadena <dacadadenadi@gmail.com>
2021-11-01 10:52:30 -0600

    [Day 1]: Learned about print, input and variables.

---
+++ DayCount/day-1/main.py
@@ -0,0 +1,48 @@
+print("Hello world!")
+
+print("\n#########  1.1 Challenge - Print #########\n")
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+
+print("\n######### String manipulation #########\n")
+print("Hello" + " " + "Daniel")
+
+print("\n#########  1.2 Challenge - String Manipulation #########\n")
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign')
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
+
+print("\n#########  Input Function #########\n")
+# print("Hello " + input("What is your name? ") + "!")
+
+print("\n#########  1.3 Challenge - Inputs #########\n")
+# print(len(input("What is your name? ")))
+
+print("\n#########  Python Variables #########\n")
+# name = input("What is your name? ")
+# length = len(name)
+# print(length)
+
+print("\n#########  1.4 Challenge - Variables #########\n")
+# a = input("a: ")
+# b = input("b: ")
+
+# middleman = a;
+# a = b;
+# b = middleman;
+
+# print("a: " + a)
+# print("b: " + b)
+
+print("#######################")
+print("##       FIRST       ##")
+print("##      PROJECT      ##")
+print("#######################\n")
+
+# Greeting
+print("Welcome to the Band Name Generator")
+city_name = input("What's the name of the city you grew up in?\n")
+pet_name = input("What's your pet's name?\n")
+print("Your band name could be " + city_name + " " + pet_name + "\n")
