819eef41139a57aeca70f6879191993b898aa015
Daniel Cadena <dacadadenadi@gmail.com>
2022-03-11 00:43:08 -0600

    [Day 5]: How to cycle on a list of items with for...in

---
+++ DayCount/day-5/main.py
@@ -0,0 +1,12 @@
+# What we're learning today:
+# Loops
+#   For loop
+
+print(f'~~~~~~~~~~ 5.1 - For Loops ~~~~~~~~~~')
+fruits = ['Apple', 'Peach', 'Pear']
+
+for fruit in fruits:
+    print(fruit)
+    print(fruit + " Pie")
+
+print(fruits)
