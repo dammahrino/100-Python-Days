2c570cac9bf141f6ead3ec855a1f7b5e94b00036
Daniel Cadena <dacadadenadi@gmail.com>
2022-03-11 00:25:07 -0600

    [Day 4]: Finished lessons and project. Learned exceptions in lists

--- DayCount/day-4/main.py
+++ DayCount/day-4/main.py
@@ -1,5 +1,9 @@
 # Randomization and lists
+from os import stat
 import random
+from turtle import pos
+
+from matplotlib.pyplot import sci
 
 print("\n#########  Using the Random function  #########\n")
 
@@ -17,7 +21,7 @@ print("\n#########  4.1 Challenge - Head or Tails?  #########\n")
 # elif value == 1:
 #     print('Tails')
 
-# print("\n#########  Data Structure : LIST #########\n")
+print("\n#########  Data Structure : LIST #########\n")
 # # Data Structure : A way to organize and store data in Python.
 # # list_name = [item1, item2, ..., itemN]
 
@@ -39,3 +43,96 @@ print("\n#########  4.2 Challenge - Random Person Picker  #########\n")
 # print(f'{names[lucky_number]} is paying for the meal today!')
 
 print("\n#########  Index Errors and Nested Lists  #########\n")
+
+# states_of_america = ['Delaware', 'Pennsylvania', 'Dakota', 'Alabama', 'New Mexico']
+# print(f'Number of States: {len(states_of_america)}')
+# try:
+#     print(states_of_america[len(states_of_america)])
+# except IndexError:
+#     print('[ERROR] You selected an index greater than the number of elements.')
+
+# fruits = ['Strawberries', 'Nectarines', 'Apples', 'Grapes', 'Peaches', 'Cherries', 'Pears']
+# vegetables = ['Spinach', 'Kale', 'Tomatoes', 'Celery', 'Potatoes']
+
+# # List of lists
+# dirty_dozen = [fruits, vegetables]
+# print(f'List of lists: {dirty_dozen}')
+
+print("\n#########  4.3 Challenge - Treasure Map  #########\n")
+
+# row1 = ["⬜️","⬜️","⬜️"]
+# row2 = ["⬜️","⬜️","⬜️"]
+# row3 = ["⬜️","⬜️","⬜️"]
+# map = [row1, row2, row3]
+# print(f"{row1}\n{row2}\n{row3}")
+# position = input("Where do you want to put the treasure? ")
+
+# second_position = int(position[0])
+# first_position = int(position[1])
+
+# map[first_position-1][second_position-1] = 'X'
+
+# print(f'{first_position} {second_position}')
+# print(f"{row1}\n{row2}\n{row3}")
+
+print("#######################")
+print("##      FOURTH       ##")
+print("##      PROJECT      ##")
+print("#######################\n")
+
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+choice = input('What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors. ')
+
+choice = int(choice)
+
+if choice >= 3 or choice < 0:
+    print('Invalid option.')
+else:
+    options = [rock, paper, scissors]
+
+    rand_option = random.randint(0, 2)
+
+    print(f'{options[choice]}\nComputer chose:\n{options[rand_option]}')
+
+    if choice == rand_option:
+        print('It\'s a tie.')
+    elif choice == 0 and rand_option == 1:
+        print('You lose.')
+    elif choice == 0 and rand_option == 2:
+        print('You win.')
+    elif choice == 1 and rand_option == 0:
+        print('You win.')
+    elif choice == 1 and rand_option == 2:
+        print('You lose.')
+    elif choice == 2 and rand_option == 0:
+        print('You lose.')
+    elif choice == 2 and rand_option == 1:
+        print('You win.')
+
+ 
