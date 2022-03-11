b0cb478b86f3d5b648b1a73af5cd370b9b290a33
Daniel Cadena <dacadadenadi@gmail.com>
2022-03-10 23:05:59 -0600

    [Day 4]: Learned about random functions and Lists

---
+++ DayCount/day-4/main.py
@@ -0,0 +1,41 @@
+# Randomization and lists
+import random
+
+print("\n#########  Using the Random function  #########\n")
+
+# random_integer = random.randint(1, 10)
+# print(random_integer)
+
+# random_float = random.random() * 5
+# print(random_float)
+
+print("\n#########  4.1 Challenge - Head or Tails?  #########\n")
+
+# value = random.randint(0,1)
+# if value == 0:
+#     print('Heads')
+# elif value == 1:
+#     print('Tails')
+
+# print("\n#########  Data Structure : LIST #########\n")
+# # Data Structure : A way to organize and store data in Python.
+# # list_name = [item1, item2, ..., itemN]
+
+# states_of_america = ['Delaware', 'Pennsylvania', 'Dakota', 'Alabama', 'New Mexico']
+
+# # Modifying an element in the list
+# # list_name[element_index] = new value
+# states_of_america[0] = 'Texas'
+
+# # Adding an element at the end of the list
+# states_of_america.append('California')
+
+print("\n#########  4.2 Challenge - Random Person Picker  #########\n")
+
+# names_string = input('Give me everybody\'s names, separated by a comma. ')
+# names = names_string.split(', ')
+# names_length = len(names)
+# lucky_number = random.randint(0,names_length - 1)
+# print(f'{names[lucky_number]} is paying for the meal today!')
+
+print("\n#########  Index Errors and Nested Lists  #########\n")
